7d295900836ea71d118459a9e4382c526fc39dcf
Virgil Dimaguila <virgild@gmail.com>
2016-05-26 18:50:14 -0400

    Updated <tr> scanning

--- page_parser.go
+++ page_parser.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -24,42 +23,16 @@ func ParseDiagnosticsPage(pagebody string) (Signals, error) {
 
 	forward_rows := doc.Find("table.light").Slice(0, 1).Find("tr")
 	forward_rows.Slice(1, forward_rows.Length()).Each(func(i int, tr *goquery.Selection) {
-		fmt.Println(tr.Html())
-		cells := tr.Find("td").Map(func(i int, td *goquery.Selection) string {
-			return td.Text()
-		})
-		channel, err := strconv.Atoi(cells[0])
-
 		var frequency, power, snr, ber float32
-		var modulation int
+		var channel, modulation int
+
+		row_str, _ := tr.Html()
+		layout := `<td align="right">%d</td><td align="right">%f MHz</td><td align="right">%f dBmV</td><td align="right">%f dB</td><td align="right">%f %%</td><td align="right">%d QAM</td>`
+		_, err := fmt.Sscanf(row_str, layout, &channel, &frequency, &power, &snr, &ber, &modulation)
 		if err != nil {
 			panic(err)
 		}
 
-		_, err = fmt.Sscanf(cells[1], "%f MHz", &frequency)
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = fmt.Sscanf(cells[2], "%f dBmV", &power)
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = fmt.Sscanf(cells[3], "%f dB", &snr)
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = fmt.Sscanf(cells[4], "%f %%", &ber)
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = fmt.Sscanf(cells[5], "%d QAM", &modulation)
-		if err != nil {
-			panic(err)
-		}
 		signals.ForwardSignals[channel] = ForwardSignal{
 			Channel:    channel,
 			Frequency:  float32(frequency),
@@ -72,33 +45,22 @@ func ParseDiagnosticsPage(pagebody string) (Signals, error) {
 
 	back_rows := doc.Find("table.light").Slice(1, 2).Find("tr")
 	back_rows.Slice(1, back_rows.Length()).Each(func(i int, tr *goquery.Selection) {
-		cells := tr.Find("td").Map(func(i int, td *goquery.Selection) string {
-			return td.Text()
-		})
-		channel, err := strconv.Atoi(cells[0])
-		if err != nil {
-			panic(err)
-		}
 		var frequency, power float32
-		var modulation int
+		var channel, modulation int
 
-		_, err = fmt.Sscanf(cells[1], "%f MHz", &frequency)
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = fmt.Sscanf(cells[2], "%f dBmV", &power)
-		if err != nil {
-			panic(err)
-		}
-
-		if strings.Contains(cells[3], "QPSK") {
-			modulation = 0
+		row_str, _ := tr.Html()
+		var layout string
+		var err error
+		if strings.Contains(row_str, "QPSK") {
+			layout = `<td align="right">%d</td><td align="right">%f MHz</td><td align="right">%f dBmV</td><td align="right">QPSK</td>`
+			_, err = fmt.Sscanf(row_str, layout, &channel, &frequency, &power)
 		} else {
-			_, err = fmt.Sscanf(cells[3], "%d QAM", &modulation)
-			if err != nil {
-				panic(err)
-			}
+			layout = `<td align="right">%d</td><td align="right">%f MHz</td><td align="right">%f dBmV</td><td align="right">%d QAM</td>`
+			_, err = fmt.Sscanf(row_str, layout, &channel, &frequency, &power, &modulation)
+		}
+
+		if err != nil {
+			panic(err)
 		}
 
 		signals.ReturnSignals[channel] = ReturnSignal{
